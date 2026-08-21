091f9a8e0d7d6bfea6e2e469f93e4132c196d021
Jessica Needham <jessin@cic-nac.hpc.uio.no>
2026-08-21 14:51:47 +0200

    Revert "update filepath to save files in scratch not home"
    This reverts commit adfb781eb024bbb7900ad33ee8413c70ea569c0c.

--- src/landusedata/luh2.py
+++ src/landusedata/luh2.py
@@ -119,12 +119,11 @@ def main(args):
     # TO DO: add check to handle if the user enters the full path
     if args.output == "LUH2_timeseries.nc":
         output_file = os.path.join(
-            #os.getcwd(),
-            "/div/no-backup-nac/users/jessica",
-            f"LUH3_timeseries_to_{args.regrid_target_file.split('/')[-1].split('.')[0]}_{datetime.now(UTC).strftime('%y%m%d')}.nc",
+            os.getcwd(),
+            f"LUH2_timeseries_to_{args.regrid_target_file.split('/')[-1].split('.')[0]}_{datetime.now(UTC).strftime('%y%m%d')}.nc",
         )
     else:
-        output_file = os.path.join("/div/no-backup-nac/users/jessica", args.output)
+        output_file = os.path.join(os.getcwd(), args.output)
 
     print(
         "Before writing output, apply capping threshold to avoid very small values from regridding"
